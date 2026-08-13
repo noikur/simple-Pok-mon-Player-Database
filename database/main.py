eb14d9590281fa268072d487b7ff3d9f29ef3dbc
Nelson Idahosa <145884395+noikur@users.noreply.github.com>
2026-08-13 03:08:32 +0100

    Add files via upload

--- database/main.py
+++ database/main.py
@@ -2,8 +2,8 @@ import sqlite3
 
 
 #create connection and cursor
-conncection = sqlite3.connect('pokemon.db')
-cursor = conncection.cursor()
+connection = sqlite3.connect('pokemon.db')
+cursor = connection.cursor()
 
 #create player table 
 cursor.execute("""
@@ -17,19 +17,55 @@ cursor.execute("""
 
 
 #player creation
-player_name = input("Enter Your Name: ")
-password_hash = input("Enter Your Password: ")
+start = input("Do you have a character(y/n):")
+if start == "n":
+    name = input("Enter Your Name: ")
+    password_hash = input("Enter Your Password: ")
 
+
+    cursor.execute("""
+                    INSERT INTO players(
+                    player_name,
+                    password_hash, 
+                    level, 
+                    experience)
+                    VALUES (?,?,?,?)
+                    """, (name, password_hash, 1, 0))
+
+    print("character created!")
+    print(f"Welcome {name}")
+
+
+elif start == "y":
+
+    name = input("Player name: ")
+
+cursor.execute("SELECT * FROM players WHERE player_name = ?", (name,))
+
+player = cursor.fetchone()
+print(player)
 
 cursor.execute("""
-                INSERT INTO players(
-                player_name,
-                password_hash, 
-                level, 
-                experience)
-                VALUES (?,?,?,?)
-                """, (player_name, password_hash, 1, 0))
+                UPDATE players
+                SET experience = experience + 100
+                WHERE player_name = ?
+                """, (name,))
 
 
-conncection.commit()
-conncection.close()
+print(f"{name} has gained exprience.")
+
+lvl = player[3]
+xp = player[4]
+
+xp_required = lvl * 100
+
+if xp >= xp_required:
+    lvl += 1
+
+
+    print(f"congratulations {name} is now level {lvl}")
+connection.commit()
+connection.close()
+
+
+
